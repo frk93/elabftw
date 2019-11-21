b9c1380f4f7aa6c9a6a388f1b1d34ff5ceb52b70
Farrukh <farrukh.faizy@gmail.com>
2019-11-21 02:52:22 +0100

    Add the button in toolbar

--- web/app/js/src/admin.js
+++ web/app/js/src/admin.js
@@ -6,43 +6,45 @@
  * @package elabftw
  */
 (function() {
-  'use strict';
+  "use strict";
 
   function tinyMceInitLight() {
     tinymce.init({
-      mode: 'specific_textareas',
-      editor_selector: 'mceditable',
-      skin_url: 'app/css/tinymce',
+      mode: "specific_textareas",
+      editor_selector: "mceditable",
+      skin_url: "app/css/tinymce",
       browser_spellcheck: true,
-      content_css: 'app/css/tinymce.css',
-      plugins: 'table searchreplace code fullscreen insertdatetime paste charmap lists advlist save image imagetools link pagebreak',
-      pagebreak_separator: '<pagebreak>',
-      toolbar1: 'undo redo | styleselect bold italic underline | alignleft aligncenter alignright alignjustify | superscript subscript | bullist numlist outdent indent | forecolor backcolor | charmap | codesample | link',
-      removed_menuitems: 'newdocument, image',
+      content_css: "app/css/tinymce.css",
+      plugins:
+        "table searchreplace code fullscreen insertdatetime paste charmap lists advlist save image imagetools link pagebreak",
+      pagebreak_separator: "<pagebreak>",
+      toolbar1:
+        "undo redo | styleselect bold italic underline | alignleft aligncenter alignright alignjustify | superscript subscript | bullist numlist outdent indent | forecolor backcolor | charmap | codesample | link",
+      removed_menuitems: "newdocument, image",
       image_caption: true,
-      language : $('#info').data('lang')
+      language: $("#info").data("lang")
     });
   }
 
   $(document).ready(function() {
-    const confirmText = $('#info').data('confirm');
+    const confirmText = $("#info").data("confirm");
 
     // activate editors in new item type and common template
     tinyMceInitLight();
 
     // TEAMGROUPS
     var TeamGroups = {
-      controller: 'app/controllers/TeamGroupsController.php',
+      controller: "app/controllers/TeamGroupsController.php",
       create: function() {
-        var name = $('#teamGroupCreate').val();
+        var name = $("#teamGroupCreate").val();
         if (name.length > 0) {
           $.post(this.controller, {
             teamGroupCreate: name
           }).done(function(json) {
             notif(json);
             if (json.res) {
-              $('#team_groups_div').load('admin.php #team_groups_div');
-              $('#teamGroupCreate').val('');
+              $("#team_groups_div").load("admin.php #team_groups_div");
+              $("#teamGroupCreate").val("");
             }
           });
         }
@@ -50,12 +52,12 @@
       update: function(action) {
         var user;
         var group;
-        if (action === 'add') {
-          user = $('#teamGroupUserAdd').val();
-          group = $('#teamGroupGroupAdd').val();
+        if (action === "add") {
+          user = $("#teamGroupUserAdd").val();
+          group = $("#teamGroupGroupAdd").val();
         } else {
-          user = $('#teamGroupUserRm').val();
-          group = $('#teamGroupGroupRm').val();
+          user = $("#teamGroupUserRm").val();
+          group = $("#teamGroupGroupRm").val();
         }
         $.post(this.controller, {
           teamGroupUpdate: true,
@@ -63,7 +65,7 @@
           teamGroupUser: user,
           teamGroupGroup: group
         }).done(function() {
-          $('#team_groups_div').load('admin.php #team_groups_div');
+          $("#team_groups_div").load("admin.php #team_groups_div");
         });
       },
       destroy: function(id) {
@@ -72,7 +74,7 @@
             teamGroupDestroy: true,
             teamGroupGroup: id
           }).done(function() {
-            $('#team_groups_div').load('admin.php #team_groups_div');
+            $("#team_groups_div").load("admin.php #team_groups_div");
           });
         }
         return false;
@@ -80,28 +82,30 @@
     };
 
     // TEAM GROUP
-    $(document).on('click', '#teamGroupCreateBtn', function() {
+    $(document).on("click", "#teamGroupCreateBtn", function() {
       TeamGroups.create();
     });
 
-    $(document).on('click', '#teamGroupGroupAddBtn', function() {
-      TeamGroups.update('add');
+    $(document).on("click", "#teamGroupGroupAddBtn", function() {
+      TeamGroups.update("add");
     });
 
-    $(document).on('click', '#teamGroupGroupRmBtn', function() {
-      TeamGroups.update('rm');
+    $(document).on("click", "#teamGroupGroupRmBtn", function() {
+      TeamGroups.update("rm");
     });
 
-    $(document).on('click', '.teamGroupDelete', function() {
-      TeamGroups.destroy($(this).data('id'));
+    $(document).on("click", ".teamGroupDelete", function() {
+      TeamGroups.destroy($(this).data("id"));
     });
 
     // VALIDATE USERS
-    $(document).on('click', '.usersValidate', function() {
-      $(this).attr('disabled', 'disabled').text('Please wait…');
-      $.post('app/controllers/UsersAjaxController.php', {
+    $(document).on("click", ".usersValidate", function() {
+      $(this)
+        .attr("disabled", "disabled")
+        .text("Please wait…");
+      $.post("app/controllers/UsersAjaxController.php", {
         usersValidate: true,
-        userid: $(this).data('userid')
+        userid: $(this).data("userid")
       }).done(function(json) {
         notif(json);
         if (json.res) {
@@ -112,16 +116,16 @@
 
     // STATUS
     var Status = {
-      controller: 'app/controllers/StatusController.php',
+      controller: "app/controllers/StatusController.php",
       create: function() {
-        var name = $('#statusName').val();
-        if (name === '') {
-          notif({'res': false, 'msg': 'Name cannot be empty'});
-          $('#statusName').css('border-color', 'red');
+        var name = $("#statusName").val();
+        if (name === "") {
+          notif({ res: false, msg: "Name cannot be empty" });
+          $("#statusName").css("border-color", "red");
           return false;
         }
-        var color = $('#statusColor').val();
-        var isTimestampable = +$('#statusTimestamp').is(':checked');
+        var color = $("#statusColor").val();
+        var isTimestampable = +$("#statusTimestamp").is(":checked");
 
         $.post(this.controller, {
           statusCreate: true,
@@ -131,15 +135,15 @@
         }).done(function(json) {
           notif(json);
           if (json.res) {
-            window.location.replace('admin.php?tab=4');
+            window.location.replace("admin.php?tab=4");
           }
         });
       },
       update: function(id) {
-        var name = $('#statusName_' + id).val();
-        var color = $('#statusColor_' + id).val();
-        var isTimestampable = +$('#statusTimestamp_'+ id).is(':checked');
-        var isDefault = $('#statusDefault_' + id).is(':checked');
+        var name = $("#statusName_" + id).val();
+        var color = $("#statusColor_" + id).val();
+        var isTimestampable = +$("#statusTimestamp_" + id).is(":checked");
+        var isDefault = $("#statusDefault_" + id).is(":checked");
 
         $.post(this.controller, {
           statusUpdate: true,
@@ -147,7 +151,7 @@
           name: name,
           color: color,
           isTimestampable: isTimestampable,
-          isDefault: isDefault ? 1 : 0,
+          isDefault: isDefault ? 1 : 0
         }).done(function(json) {
           notif(json);
         });
@@ -159,40 +163,40 @@
         }).done(function(json) {
           notif(json);
           if (json.res) {
-            $('#status_' + id).hide();
+            $("#status_" + id).hide();
           }
         });
       }
     };
-    $(document).on('click', '#statusCreate', function() {
+    $(document).on("click", "#statusCreate", function() {
       Status.create();
     });
 
-    $(document).on('click', '.statusSave', function() {
-      Status.update($(this).data('id'));
+    $(document).on("click", ".statusSave", function() {
+      Status.update($(this).data("id"));
     });
 
-    $(document).on('click', '.statusDestroy', function() {
-      Status.destroy($(this).data('id'));
+    $(document).on("click", ".statusDestroy", function() {
+      Status.destroy($(this).data("id"));
     });
 
     // ITEMSTYPES
     var ItemsTypes = {
-      controller: 'app/controllers/ItemsTypesAjaxController.php',
+      controller: "app/controllers/ItemsTypesAjaxController.php",
       create: function() {
-        var name = $('#itemsTypesName').val();
-        if (name === '') {
-          notif({'res': false, 'msg': 'Name cannot be empty'});
-          $('#itemsTypesName').css('border-color', 'red');
+        var name = $("#itemsTypesName").val();
+        if (name === "") {
+          notif({ res: false, msg: "Name cannot be empty" });
+          $("#itemsTypesName").css("border-color", "red");
           return false;
         }
-        var color = $('#itemsTypesColor').val();
-        var checkbox = $('#itemsTypesBookable').is(':checked');
+        var color = $("#itemsTypesColor").val();
+        var checkbox = $("#itemsTypesBookable").is(":checked");
         var bookable = 0;
         if (checkbox) {
           bookable = 1;
         }
-        var template = tinymce.get('itemsTypesTemplate').getContent();
+        var template = tinymce.get("itemsTypesTemplate").getContent();
         $.post(this.controller, {
           itemsTypesCreate: true,
           name: name,
@@ -202,30 +206,30 @@
         }).done(function(json) {
           notif(json);
           if (json.res) {
-            window.location.replace('admin.php?tab=5');
+            window.location.replace("admin.php?tab=5");
           }
         });
       },
       showEditor: function(id) {
-        $('#itemsTypesTemplate_' + id).addClass('mceditable');
+        $("#itemsTypesTemplate_" + id).addClass("mceditable");
         tinyMceInitLight();
-        $('#itemsTypesEditor_' + id).toggle();
+        $("#itemsTypesEditor_" + id).toggle();
       },
       update: function(id) {
-        var name = $('#itemsTypesName_' + id).val();
-        var color = $('#itemsTypesColor_' + id).val();
-        var checkbox = $('#itemsTypesBookable_' + id).is(':checked');
+        var name = $("#itemsTypesName_" + id).val();
+        var color = $("#itemsTypesColor_" + id).val();
+        var checkbox = $("#itemsTypesBookable_" + id).is(":checked");
         var bookable = 0;
         if (checkbox) {
           bookable = 1;
         }
         // if tinymce is hidden, it'll fail to trigger
         // so we toggle it quickly to grab the content
-        if ($('#itemsTypesTemplate_' + id).is(':hidden')) {
+        if ($("#itemsTypesTemplate_" + id).is(":hidden")) {
           this.showEditor(id);
         }
-        var template = tinymce.get('itemsTypesTemplate_' + id).getContent();
-        $('#itemsTypesEditor_' + id).toggle();
+        var template = tinymce.get("itemsTypesTemplate_" + id).getContent();
+        $("#itemsTypesEditor_" + id).toggle();
 
         $.post(this.controller, {
           itemsTypesUpdate: true,
@@ -245,77 +249,86 @@
         }).done(function(json) {
           notif(json);
           if (json.res) {
-            $('#itemstypes_' + id).hide();
-            $('#itemstypesOrder_' + id).hide();
+            $("#itemstypes_" + id).hide();
+            $("#itemstypesOrder_" + id).hide();
           }
         });
       }
     };
 
-    $('.itemsTypesEditor').hide();
-    $(document).on('click', '#itemsTypesCreate', function() {
+    $(".itemsTypesEditor").hide();
+    $(document).on("click", "#itemsTypesCreate", function() {
       ItemsTypes.create();
     });
-    $(document).on('click', '.itemsTypesShowEditor', function() {
-      ItemsTypes.showEditor($(this).data('id'));
+    $(document).on("click", ".itemsTypesShowEditor", function() {
+      ItemsTypes.showEditor($(this).data("id"));
     });
-    $(document).on('click', '.itemsTypesUpdate', function() {
-      ItemsTypes.update($(this).data('id'));
+    $(document).on("click", ".itemsTypesUpdate", function() {
+      ItemsTypes.update($(this).data("id"));
     });
-    $(document).on('click', '.itemsTypesDestroy', function() {
-      ItemsTypes.destroy($(this).data('id'));
+    $(document).on("click", ".itemsTypesDestroy", function() {
+      ItemsTypes.destroy($(this).data("id"));
     });
 
     // COMMON TEMPLATE
-    $('#commonTplTemplate').closest('div').find('.button').click(function() {
-      var template = tinymce.get('commonTplTemplate').getContent();
-      $.post('app/controllers/AjaxController.php', {
-        commonTplUpdate: template
-      }).done(function(json) {
-        notif(json);
+    $("#commonTplTemplate")
+      .closest("div")
+      .find(".button")
+      .click(function() {
+        var template = tinymce.get("commonTplTemplate").getContent();
+        $.post("app/controllers/AjaxController.php", {
+          commonTplUpdate: template
+        }).done(function(json) {
+          notif(json);
+        });
       });
-    });
 
     // validate on enter
-    $('#teamGroupCreate').keypress(function (e) {
+    $("#teamGroupCreate").keypress(function(e) {
       let keynum;
       if (e.which) {
         keynum = e.which;
       }
-      if (keynum === 13) { // if the key that was pressed was Enter (ascii code 13)
+      if (keynum === 13) {
+        // if the key that was pressed was Enter (ascii code 13)
         TeamGroups.create();
       }
     });
     // edit the team group name
-    $('h3.teamgroup_name').editable('app/controllers/TeamGroupsController.php', {
-      indicator : 'Saving...',
-      name : 'teamGroupUpdateName',
-      submit : 'Save',
-      cancel : 'Cancel',
-      cancelcssclass : 'button button-delete',
-      submitcssclass : 'button',
-      style : 'display:inline'
-
-    });
+    $("h3.teamgroup_name").editable(
+      "app/controllers/TeamGroupsController.php",
+      {
+        indicator: "Saving...",
+        name: "teamGroupUpdateName",
+        submit: "Save",
+        cancel: "Cancel",
+        cancelcssclass: "button button-delete",
+        submitcssclass: "button",
+        style: "display:inline"
+      }
+    );
 
     // randomize the input of the color picker so even if user doesn't change the color it's a different one!
     // from https://www.paulirish.com/2009/random-hex-color-code-snippets/
-    var colorInput = '#' + Math.floor(Math.random()*16777215).toString(16);
-    $('.randomColor').val(colorInput);
+    var colorInput = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    $(".randomColor").val(colorInput);
 
-    $('.tag-editable').editable(function(value) {
-      $.post('app/controllers/TagsController.php', {
-        update: true,
-        newtag: value,
-        tag: $(this).data('tag')
-      });
+    $(".tag-editable").editable(
+      function(value) {
+        $.post("app/controllers/TagsController.php", {
+          update: true,
+          newtag: value,
+          tag: $(this).data("tag")
+        });
 
-      return(value);
-    }, {
-      tooltip : 'Click to edit',
-      indicator : 'Saving...',
-      onblur: 'submit',
-      style : 'display:inline'
-    });
+        return value;
+      },
+      {
+        tooltip: "Click to edit",
+        indicator: "Saving...",
+        onblur: "submit",
+        style: "display:inline"
+      }
+    );
   });
-}());
+})();
--- web/app/js/src/sysconfig.js
+++ web/app/js/src/sysconfig.js
@@ -6,15 +6,15 @@
  * @package elabftw
  */
 (function() {
-  'use strict';
+  "use strict";
 
   $(document).ready(function() {
     // TEAMS
     var Teams = {
-      controller: 'app/controllers/SysconfigAjaxController.php',
+      controller: "app/controllers/SysconfigAjaxController.php",
       create: function() {
-        document.getElementById('teamsCreateButton').disabled = true;
-        var name = $('#teamsName').val();
+        document.getElementById("teamsCreateButton").disabled = true;
+        var name = $("#teamsName").val();
         $.post(this.controller, {
           teamsCreate: true,
           teamsName: name
@@ -23,20 +23,20 @@
         });
       },
       update: function(id) {
-        document.getElementById('teamsUpdateButton_' + id).disabled = true;
-        var name = $('#teamName_' + id).val();
-        var orgid = $('#teamOrgid_' + id).val();
+        document.getElementById("teamsUpdateButton_" + id).disabled = true;
+        var name = $("#teamName_" + id).val();
+        var orgid = $("#teamOrgid_" + id).val();
         $.post(this.controller, {
           teamsUpdate: true,
-          teamsUpdateId : id,
-          teamsUpdateName : name,
-          teamsUpdateOrgid : orgid
+          teamsUpdateId: id,
+          teamsUpdateName: name,
+          teamsUpdateOrgid: orgid
         }).done(function(data) {
           Teams.destructor(data);
         });
       },
       destroy: function(id) {
-        document.getElementById('teamsDestroyButton_' + id).disabled = true;
+        document.getElementById("teamsDestroyButton_" + id).disabled = true;
         $.post(this.controller, {
           teamsDestroy: true,
           teamsDestroyId: id
@@ -47,130 +47,134 @@
       destructor: function(json) {
         notif(json);
         if (json.res) {
-          $('#teamsDiv').load('sysconfig.php #teamsDiv');
+          $("#teamsDiv").load("sysconfig.php #teamsDiv");
         }
       }
     };
 
-    $(document).on('keyup', '.teamNameInput', function() {
-      document.getElementById('teamsUpdateButton_' + $(this).data('id')).disabled = false;
+    $(document).on("keyup", ".teamNameInput", function() {
+      document.getElementById(
+        "teamsUpdateButton_" + $(this).data("id")
+      ).disabled = false;
     });
 
-    $(document).on('click', '#teamsCreateButton', function() {
+    $(document).on("click", "#teamsCreateButton", function() {
       Teams.create();
     });
-    $(document).on('click', '.teamsUpdateButton', function() {
-      Teams.update($(this).data('id'));
+    $(document).on("click", ".teamsUpdateButton", function() {
+      Teams.update($(this).data("id"));
     });
-    $(document).on('click', '.teamsDestroyButton', function() {
-      Teams.destroy($(this).data('id'));
+    $(document).on("click", ".teamsDestroyButton", function() {
+      Teams.destroy($(this).data("id"));
     });
-    $(document).on('click', '.teamsArchiveButton', function() {
-      notif({'msg': 'Feature not yet implemented :)', 'res': true});
+    $(document).on("click", ".teamsArchiveButton", function() {
+      notif({ msg: "Feature not yet implemented :)", res: true });
     });
 
     // MAIL METHOD in a function because is also called in document ready
     function toggleMailMethod(method) {
       switch (method) {
-      case 'sendmail':
-        $('#smtp_config').hide();
-        $('#sendmail_config').show();
-        break;
-      case 'smtp':
-        $('#smtp_config').show();
-        $('#sendmail_config').hide();
-        break;
-      default:
-        $('#smtp_config').hide();
-        $('#sendmail_config').hide();
-        $('#general_mail_config').hide();
+        case "sendmail":
+          $("#smtp_config").hide();
+          $("#sendmail_config").show();
+          break;
+        case "smtp":
+          $("#smtp_config").show();
+          $("#sendmail_config").hide();
+          break;
+        default:
+          $("#smtp_config").hide();
+          $("#sendmail_config").hide();
+          $("#general_mail_config").hide();
       }
     }
-    $(document).on('change', '#selectMailMethod', function() {
+    $(document).on("change", "#selectMailMethod", function() {
       toggleMailMethod($(this).val());
     });
 
     // MASS MAIL
-    $(document).on('click', '#massSend', function() {
-      $('#massSend').prop('disabled', true);
-      $('#massSend').text('Sending…');
-      $.post('app/controllers/SysconfigAjaxController.php', {
+    $(document).on("click", "#massSend", function() {
+      $("#massSend").prop("disabled", true);
+      $("#massSend").text("Sending…");
+      $.post("app/controllers/SysconfigAjaxController.php", {
         massEmail: true,
-        subject: $('#massSubject').val(),
-        body: $('#massBody').val()
+        subject: $("#massSubject").val(),
+        body: $("#massBody").val()
       }).done(function(json) {
         notif(json);
         if (json.res) {
-          $('#massSend').text('Sent!');
+          $("#massSend").text("Sent!");
         } else {
-          $('#massSend').prop('disabled', false);
-          $('#massSend').css('background-color', '#e6614c');
-          $('#massSend').text('Error');
+          $("#massSend").prop("disabled", false);
+          $("#massSend").css("background-color", "#e6614c");
+          $("#massSend").text("Error");
         }
       });
     });
 
     // TEST EMAIL
-    $(document).on('click', '#testemailButton', function() {
-      var email = $('#testemailEmail').val();
-      document.getElementById('testemailButton').disabled = true;
-      $('#testemailButton').text('Sending…');
-      $.post('app/controllers/SysconfigAjaxController.php', {
+    $(document).on("click", "#testemailButton", function() {
+      var email = $("#testemailEmail").val();
+      document.getElementById("testemailButton").disabled = true;
+      $("#testemailButton").text("Sending…");
+      $.post("app/controllers/SysconfigAjaxController.php", {
         testemailSend: true,
         testemailEmail: email
       }).done(function(json) {
         notif(json);
         if (json.res) {
-          $('#massSend').text('Sent!');
-          document.getElementById('testemailButton').disabled = false;
+          $("#massSend").text("Sent!");
+          document.getElementById("testemailButton").disabled = false;
         } else {
-          $('#testemailButton').text('Error');
-          $('#testemailButton').css('background-color', '#e6614c');
+          $("#testemailButton").text("Error");
+          $("#testemailButton").css("background-color", "#e6614c");
         }
       });
     });
 
-    $(document).on('click', '#editSmtpPassword', function() {
-      $('#hidden_smtp_password').toggle();
+    $(document).on("click", "#editSmtpPassword", function() {
+      $("#hidden_smtp_password").toggle();
     });
 
     // we need to add this otherwise the button will stay disabled with the browser's cache (Firefox)
-    var input_list = document.getElementsByTagName('input');
-    for (var i=0; i < input_list.length; i++) {
+    var input_list = document.getElementsByTagName("input");
+    for (var i = 0; i < input_list.length; i++) {
       var input = input_list[i];
       input.disabled = false;
     }
     // honor already saved mail_method setting and hide unused options accordingly
-    toggleMailMethod($('#selectMailMethod').val());
+    toggleMailMethod($("#selectMailMethod").val());
 
-    $(document).on('click', '.idpsDestroy', function() {
+    $(document).on("click", ".idpsDestroy", function() {
       const elem = $(this);
-      if (confirm($(this).data('confirm'))) {
-        $.post('app/controllers/SysconfigAjaxController.php', {
+      if (confirm($(this).data("confirm"))) {
+        $.post("app/controllers/SysconfigAjaxController.php", {
           idpsDestroy: true,
-          id: $(this).data('id')
+          id: $(this).data("id")
         }).done(function(json) {
           notif(json);
           if (json.res) {
-            elem.closest('div').hide(600);
+            elem.closest("div").hide(600);
           }
         });
       }
     });
 
     tinymce.init({
-      mode: 'specific_textareas',
-      editor_selector: 'mceditable',
+      mode: "specific_textareas",
+      editor_selector: "mceditable",
       browser_spellcheck: true,
-      skin_url: 'app/css/tinymce',
-      height: '500',
-      plugins: 'table searchreplace code fullscreen insertdatetime paste charmap lists advlist save image imagetools link pagebreak hr',
-      pagebreak_separator: '<pagebreak>',
-      toolbar1: 'undo redo | styleselect bold italic underline | alignleft aligncenter alignright alignjustify | superscript subscript | bullist numlist outdent indent | forecolor backcolor | charmap | codesample | link',
-      removed_menuitems: 'newdocument, image',
+      skin_url: "app/css/tinymce",
+      height: "500",
+      plugins:
+        "table searchreplace code fullscreen insertdatetime paste charmap lists advlist save image imagetools link pagebreak hr",
+      pagebreak_separator: "<pagebreak>",
+      toolbar1:
+        "undo redo | styleselect bold italic underline | alignleft aligncenter alignright alignjustify | superscript subscript | bullist numlist outdent indent | forecolor backcolor | charmap | codesample | link",
+      removed_menuitems: "newdocument, image",
       image_caption: false,
-      content_style: '.mce-content-body {font-size:10pt;}',
-      language: 'en_GB'
+      content_style: ".mce-content-body {font-size:10pt;}",
+      language: "en_GB"
     });
   });
-}());
+})();
